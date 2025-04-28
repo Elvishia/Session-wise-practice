f223bde9482e72a59ba61f0ad6e18dc00153ab92
Elvishia <elliecrasto@gmail.com>
2025-04-28 20:01:55 +0530

    second commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,8 @@ create table courses(course_id int, name varchar(30))
 alter table student add cours_id int 
 insert into student values (7,'mannan',52,25,105),(8,'mayuri',60,23,101),(9,'raj',69,30,103),(10,'karishma',60,21,101)
 insert into courses values(101,'IT'),(102,'data_science'),(103,'data_engineer'),(105,'accounts'),(106,'finance')
+select * from institutes
+create database elvishia
+select * from elvishia
+use elvishia
+create table customer (rollno int primary key, name varchar(20))
